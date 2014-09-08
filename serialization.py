b26f5656ecfa644afa742e167d0d22d5ba436992
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-09-08 17:41:27 +0200

    Traversion works. Must refactor

--- serialization.py
+++ serialization.py
@@ -2,88 +2,121 @@ from datetime import datetime
 import itertools
 from marshmallow import Serializer, fields, pprint
 
+class Embedded(fields.Nested):
+    pass
+
+class Linked(fields.Nested):
+    pass
 
 class BaseSerializer(Serializer):
     pass
 
 @BaseSerializer.data_handler
 def jsonapify(serializer, data, obj):
-    def parse(obj, keys, serializer):
-        key = keys[0]
-        if isinstance(obj, list):
-            lst = [parse(e, keys, serializer) for e in obj]
-            if isinstance(lst[0], list):
-                return list(itertools.chain.from_iterable(lst))
-            else:
-                return lst
-        if len(keys) == 1:
-            raw = obj[key]
-            if isinstance(raw, list):
-                return [serializer(e).data for e in raw]
-            else:
-                return serializer(raw).data
-        else:
-            return parse(obj[key], keys[1:], serializer)
+    def get_data(schema, data):
+        k = schema.Meta.primary_key
+        n = schema.Meta.name
+        return {n: {data[k]: data}}
 
-    def get_linked(obj, linked):
-        return {key: parse(obj, obje['key'].split("."), obje['serializer']) for key, obje in linked.items()}
+    def update_map(a, b):
+        for k, v in b.items():
+            if k in a:
+                a[k].update(v)
+            else:
+                a[k] = v
+        return a.copy()
+
+    def depth_first(schema, data):
+        linked = dict()
+        for field in schema.declared_fields.values():
+            name = field.name
+            if isinstance(field, fields.Nested):
+                if isinstance(field, Embedded):
+                    if field.many:
+                        for e in data[name]:
+                            for f in field.schema.fields.values():
+                                if isinstance(f, Linked):
+                                    if f.many:
+                                        links = {f.schema.Meta.name: [o[f.schema.Meta.primary_key] for o in e[f.schema.Meta.name]]}
+                                    else:
+                                        links = {f.schema.Meta.name: e[f.schema.Meta.single_name][f.schema.Meta.primary_key]}
+                                    if 'links' in e:
+                                        e['links'].update(links)
+                                    else:
+                                        e['links'] = links
+
+                if field.many:
+                    for e in data[name]:
+                        p = depth_first(field.schema, e)
+                        linked = update_map(linked, p)
+                else:
+                    p = depth_first(field.schema, data[name])
+                    linked = update_map(linked, p)
+                if isinstance(field, Linked):
+                    del data[name]
+
+        return update_map(linked, get_data(schema, data))
+
+
+    linked = depth_first(serializer, data)
+
+    for key, val in linked.items():
+        linked[key] = val.values()
+
+    del linked[serializer.ROOT]
 
     return {
         serializer.ROOT: data,
-        "linked": get_linked(obj, serializer.LINKED)
+        "linked": linked
     }
 
 
 class EventTypeSerializer(Serializer):
     class Meta():
+        primary_key = 'event_type_id'
+        name = 'event_types'
         additional = ('event_type_id', 'organization_id')
 
 
 class EventSerializer(Serializer):
     class Meta():
+        primary_key = 'event_id'
+        name = 'events'
+        single_name = 'event'
         additional = ('event_id',)
+
+    event_type = Linked(EventTypeSerializer)
 
 
 class TicketTypeSerializer(Serializer):
     class Meta():
+        primary_key = 'ticket_type_id'
+        name = 'ticket_types'
+        single_name = 'ticket_type'
         additional = ("vat_factor", "name", "price", "ticket_type_id", "event_type_id", "identifier", "data")
 
 
 class TicketReservationSerializer(Serializer):
-    links = fields.Method('get_links')
-
-    def get_links(self, obj):
-        return {
-            'ticket_type': obj['ticket_type']['ticket_type_id'],
-            'event': obj['event_id']
-        }
-
     class Meta:
+        primary_key = 'uuid'
+        name = 'tickets'
         additional = ('price', 'fee', 'vat', 'fee_vat', 'uuid', 'data')
+
+    events = Linked(EventSerializer, many=True)
+    ticket_type = Linked(TicketTypeSerializer)
 
 
 class ReservationSerializer(BaseSerializer):
+    class Meta:
+        primary_key = 'reservation_id'
+        name = 'reservations'
+        additional = ('data', 'reservation_id')
+
     ROOT = 'reservations'
-    LINKED = {
-        'ticket_types': {
-            'key': 'tickets.ticket_type',
-            'serializer': TicketTypeSerializer
-        },
-        'event_types': {
-            'key': 'tickets.events.event_type',
-            'serializer': EventTypeSerializer
-        },
-        'events': {
-            "key": 'tickets.events',
-            "serializer": EventSerializer
-        },
-    }
     last_touched = fields.DateTime(format='%Y-%m-%dT%H:%M:%S')
     created = fields.DateTime(format='%Y-%m-%dT%H:%M:%S')
-    ticket_reservations = fields.Nested(TicketReservationSerializer, attribute='tickets', many=True)
+    ticket_reservations = Embedded(TicketReservationSerializer, attribute='tickets', many=True)
 
-    class Meta:
-        additional = ('data', 'reservation_id')
 
 
 reservation = {
