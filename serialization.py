0bb8e1c8074f3d449ed9ac44656c82489b07fadc
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-09-09 14:44:58 +0200

    Move help functions out of data_handler

--- serialization.py
+++ serialization.py
@@ -15,74 +15,79 @@ class Serializer(MarshmallowSerializer):
     def nested_fields(self):
         return [field for field in self.fields.values() if isinstance(field, fields.Nested)]
 
+
+def _add_links_from_field(field, data):
+    if not isinstance(field, Linked):
+        return
+
+    plural_name = field.schema.Meta.plural_name
+    primary_key = field.schema.Meta.primary_key
+    name = field.schema.Meta.name
+    if field.many:
+        links = {plural_name: [o[primary_key] for o in data[plural_name]]}
+    else:
+        links = {plural_name: data[name][primary_key]}
+    if 'links' in data:
+        data['links'].update(links)
+    else:
+        data['links'] = links
+
+
+def _add_links_from_schema(schema, data):
+    for field in schema.nested_fields:
+        _add_links_from_field(field, data)
+
+        name = field.name
+
+        for _data in data[name] if field.many else [data[name]]:
+            _add_links_from_schema(field.schema, _data)
+
+
+def _get_data(schema, data):
+    k = schema.Meta.primary_key
+    n = schema.Meta.plural_name
+    return {n: {data[k]: data}}
+
+
+def _update_map(a, b):
+    for k, v in b.items():
+        if k in a:
+            a[k].update(v)
+        else:
+            a[k] = v
+
+
+def _recur_schema(schema, data, linked):
+    for field in schema.nested_fields:
+        _recur_field(field, data, linked)
+
+
+def _recur_field(field, data, linked):
+    name = field.name
+
+    for _data in data[name] if field.many else [data[name]]:
+        _recur_schema(field.schema, _data, linked)
+        if isinstance(field, Linked):
+            _update_map(linked, _get_data(field.schema, _data))
+
+    if isinstance(field, Linked):
+        del data[name]
+
+
 @Serializer.data_handler
 def jsonapify(serializer, data, obj):
-    def add_links_from_field(field, data):
-        if not isinstance(field, Linked):
-            return
 
-        plural_name = field.schema.Meta.plural_name
-        primary_key = field.schema.Meta.primary_key
-        name = field.schema.Meta.name
-        if field.many:
-            links = {plural_name: [o[primary_key] for o in data[plural_name]]}
-        else:
-            links = {plural_name: data[name][primary_key]}
-        if 'links' in data:
-            data['links'].update(links)
-        else:
-            data['links'] = links
-
-    def add_links_form_schema(schema, data):
-        for field in schema.nested_fields:
-            add_links_from_field(field, data)
-
-            name = field.name
-            if field.many:
-                for e in data[name]:
-                    add_links_form_schema(field.schema, e)
-            else:
-                add_links_form_schema(field.schema, data[name])
+    def add_links():
+        _add_links_from_schema(serializer, data)
 
     def get_linked():
-        def get_data(schema, data):
-            k = schema.Meta.primary_key
-            n = schema.Meta.plural_name
-            return {n: {data[k]: data}}
-
-        def update_map(a, b):
-            for k, v in b.items():
-                if k in a:
-                    a[k].update(v)
-                else:
-                    a[k] = v
-
-        def recur_schema(schema, data, linked):
-            for field in schema.nested_fields:
-                recur_field(field, data, linked)
-
-        def recur_field(field, data, linked):
-            name = field.name
-            if field.many:
-                for schema_data in data[name]:
-                    recur_schema(field.schema, schema_data, linked)
-                    if isinstance(field, Linked):
-                        update_map(linked, get_data(field.schema, schema_data))
-            else:
-                recur_schema(field.schema, data[name], linked)
-                if isinstance(field, Linked):
-                    update_map(linked, get_data(field.schema, data[name]))
-
-            if isinstance(field, Linked):
-                del data[name]
-
         linked = dict()
-        recur_schema(serializer, data, linked)
+        _recur_schema(serializer, data, linked)
         for key, val in linked.items():
             linked[key] = val.values()
         return linked
 
-    add_links_form_schema(serializer, data)
+    add_links()
     return {
         serializer.ROOT: data,
         "linked": get_linked()
